9016752b0b07ca196865c0a71c63bb9b31b0ff56
JamesODonoghue <jodonogh1@gmail.com>
2020-04-03 04:59:04 +0000

    Deploying to gh-pages from  @ e41597b3c17d7a51507f56b5450665ba92631e8c 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1254d91e0abcf62872cd9155a899f223.js"
+  "/precache-manifest.0675b6184847c37f6443f811012ac708.js"
 );
 
 self.addEventListener('message', (event) => {
