b43961e36b461f6ca89ba5dac9fc7f187e301373
James O'Donoghue <jodonogh1@gmail.com>
2020-04-03 07:00:52 -0700

    date update

--- src/components/ZoomClass/index.tsx
+++ src/components/ZoomClass/index.tsx
@@ -4,8 +4,8 @@ export const ZoomClass = () => (
     <section className="zoom-section">
         <h1 style={{ textAlign: "center" }}>Calling All Kids!</h1>
         <p>
-            Ms. Liza will offer fun, 35-minute dance classes on Zoom from April
-            6 - April 10 at 4pm Pacfic Time.
+            Ms. Liza will offer fun, 35-minute dance classes on Zoom from March
+            30 - April 4 at 4pm Pacfic Time.
         </p>
         <p>
             Get the wiggles out after staying home safe all day! No previous
