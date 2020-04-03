03f189ace5d9e5fb6e5505dacbfc4f0a829d598b
James O'Donoghue <jodonogh1@gmail.com>
2020-04-02 22:11:25 -0700

    updated some dates

--- src/components/ZoomClass/index.tsx
+++ src/components/ZoomClass/index.tsx
@@ -4,8 +4,8 @@ export const ZoomClass = () => (
     <section className="zoom-section">
         <h1 style={{ textAlign: "center" }}>Calling All Kids!</h1>
         <p>
-            Ms. Liza will offer fun, 35-minute dance classes on Zoom from March
-            30-April 4 at 4pm Pacfic Time.
+            Ms. Liza will offer fun, 35-minute dance classes on Zoom from April
+            6 - April 10 at 4pm Pacfic Time.
         </p>
         <p>
             Get the wiggles out after staying home safe all day! No previous
@@ -18,7 +18,10 @@ export const ZoomClass = () => (
         <p>
             <b>Ages 3-7: Tuesday, Thursday</b>
         </p>
-        <p>Venmo donations @LizaKroeschell appreciated</p>
+
+        <p style={{ marginTop: "5rem" }}>
+            Venmo donations @LizaKroeschell appreciated
+        </p>
 
         <div style={{ marginTop: "7rem" }}>
             <a href="mailto:liza.kroeschell@gmail.com?subject=Zoom Dance Class">
