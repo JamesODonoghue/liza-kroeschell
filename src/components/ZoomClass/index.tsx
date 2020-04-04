b55703f0a55bfb20f5839833ade86d928a2a6548
James O'Donoghue <jodonogh1@gmail.com>
2020-04-04 09:25:25 -0700

    updated the zoom info, updated the landing background

--- src/components/ZoomClass/index.tsx
+++ src/components/ZoomClass/index.tsx
@@ -3,28 +3,46 @@ import React from "react"
 export const ZoomClass = () => (
     <section className="zoom-section">
         <h1 style={{ textAlign: "center" }}>Calling All Kids!</h1>
+        <h4 style={{ fontWeight: "bold" }}>
+            Ms. Liza teaches weekly open dance classes and is available for
+            private lessons upon request.
+        </h4>
         <p>
-            Ms. Liza will offer fun, 35-minute dance classes on Zoom from March
-            30 - April 4 at 4pm Pacfic Time.
+            <b>Open classes: Tuesdays and Thursdays at 4pm Pacfic Time.</b>
         </p>
         <p>
-            Get the wiggles out after staying home safe all day! No previous
-            dance experience necessary.
+            Join Ms. Liza for fun, 45-minute dance classes on Zoom. Get the
+            wiggles out after staying home safe all day!
+        </p>
+        <p>
+            Each class will include an imaginative activity for children as
+            young as three and a challenge step for those as old as ten. No
+            previous dance experience necessary. All ages welcome!
+        </p>
+        <p>
+            <i>Venmo donations @Liza-Kroeschell appreciated.</i>
         </p>
 
-        <p>Find a safe space to move and email me for the Zoom link below!</p>
+        <div>
+            <a href="mailto:liza.kroeschell@gmail.com?subject=Zoom Open Dance Class">
+                <button className="secondary">
+                    Email me for the Zoom Link
+                </button>
+            </a>
+        </div>
         <p>
-            <b>Ages 5-10: Monday, Wednesday, Friday</b>
+            <b>Private Lessons</b>: Want to Dance MORE? Ms. Liza teaches private
+            and small group dance lessons on Zoom. Pay what you can!
         </p>
         <p>
-            <b>Ages 3-7: Tuesday, Thursday</b>
+            Email <b>liza.kroeschell@gmail.com</b> for more information about
+            individualized dance instruction tailored to your child’s interests
+            and goals.
         </p>
 
-        <p>Venmo donations @LizaKroeschell appreciated</p>
-
-        <div style={{ marginTop: "5rem" }}>
-            <a href="mailto:liza.kroeschell@gmail.com?subject=Zoom Dance Class">
-                <button>Email me for the Zoom link</button>
+        <div>
+            <a href="mailto:liza.kroeschell@gmail.com?subject=Zoom Private Class">
+                <button>Schedule a private class!</button>
             </a>
         </div>
     </section>
