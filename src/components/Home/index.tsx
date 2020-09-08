d76d3aade2c654a92aa986748aaae1a5841ff478
James O'Donoghue <jodonogh1@gmail.com>
2020-09-07 21:28:59 -0700

    updated some styles

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -10,14 +10,14 @@ export const Home = () => {
     return (
         <Fragment>
             <Landing />
-            <div className="h-12 lg:h-32 rounded-b-50 max-w-full w-screen bg-white absolute"></div>
+            <div className="h-16 lg:h-24 rounded-b-50 max-w-full w-screen bg-white absolute"></div>
             <Testimonials />
-            <div className="h-12 lg:h-32 rounded-b-50 max-w-full w-screen bg-teal-100"></div>
+            <div className="h-16 lg:h-24 rounded-b-50 max-w-full w-screen bg-teal-100"></div>
             {/* <About /> */}
             {/* <AboutNext /> */}
             <Creativity />
             <Confidence />
-            <div className="h-12 lg:h-32 rounded-b-50 max-w-full w-screen bg-white absolute"></div>
+            <div className="h-16 lg:h-24 rounded-b-50 max-w-full w-screen bg-white absolute"></div>
             {/* <Experience /> */}
             <Footer />
         </Fragment>
--- src/components/Testimonials/index.tsx
+++ src/components/Testimonials/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext, DotGroup, Dot } from 'pure-react-carousel';
 export const Testimonials = () => {
     return (
-        <section className="px-8 pt-64 bg-teal-100">
+        <section className="px-8 pt-32 sm:pt-64 bg-teal-100">
             <CarouselProvider
                 infinite
                 isIntrinsicHeight={true}
@@ -88,9 +88,9 @@ export const Testimonials = () => {
                             </div>
                         );
                     }}
-                    className="max-w-sm mx-auto flex justify-between mt-16"
+                    className="max-w-sm mx-auto flex justify-between mt-8 sm:mt-16"
                 ></DotGroup>
-                <div className="max-w-sm mx-auto flex justify-between mt-16">
+                <div className="max-w-sm mx-auto flex justify-between mt-8 sm:mt-16">
                     <ButtonBack className="focus:outline-none focus:shadow-outline uppercase tracking-wider font-semibold text-indigo-900 px-4 py-2 hover:bg-teal-200 rounded-md transition duration-200">
                         Previous
                     </ButtonBack>
