378853d3ffda57d00e217497d3708c405565b553
James O'Donoghue <jodonogh1@gmail.com>
2020-09-07 21:11:01 -0700

    fixed removed css

--- postcss.config.js
+++ postcss.config.js
@@ -3,7 +3,7 @@ module.exports = {
         require('tailwindcss'),
         require('autoprefixer'),
         require('@fullhuman/postcss-purgecss')({
-            content: ['./src/**/*.tsx', './public/index.html'],
+            content: ['./src/**/*.tsx', './public/index.html', './src/tailwind.css'],
             defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g),
         }),
     ],
